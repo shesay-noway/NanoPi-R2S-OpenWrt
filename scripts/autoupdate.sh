79f3d631c54546a44d66d2b0ffebae78e71c7370
msylgj <msylgj@vip.qq.com>
2020-05-01 12:09:36 +0800

    刷机使用overlay分区,不依赖自动挂载

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -13,8 +13,8 @@ read -p '请选择(1/2/3/4):' input
 if [ $input -eq 3 ] || [ $input -eq 4 ]; then
 	if [ -f /tmp/upload/R2S*.zip ]; then
 		echo -e '\e[92m已找到固件文件，准备解压\e[0m'
-		mv /tmp/upload/R2S*.zip /mnt/mmcblk0p2
-		cd /mnt/mmcblk0p2
+		mv /tmp/upload/R2S*.zip /overlay
+		cd /overlay
 		if [ $input -eq 4 ]; then
 			backup=0
 		fi
@@ -45,15 +45,15 @@ else
 		echo -e '\e[92m从Github主站下载\e[0m'
 	fi
 	# 下载固件
-	cd /mnt/mmcblk0p2
+	cd /overlay
 	rm -rf artifact R2S*.zip FriendlyWrt*img*
 	wget $url/msylgj/NanoPi-R2S-OpenWrt/releases/download/R2S-OPoA-$romtype-$(date +%Y-%m-%d)/R2S-OPoA-$romtype-$(date +%Y-%m-%d)-ROM.zip
-	if [ -f /mnt/mmcblk0p2/R2S*.zip ]; then
+	if [ -f /overlay/R2S*.zip ]; then
 		echo -e '\e[92m今天固件已下载，准备解压\e[0m'
 	else
 		echo -e '\e[91m今天的固件还没更新，尝试下载昨天的固件\e[0m'
 		wget $url/msylgj/NanoPi-R2S-OpenWrt/releases/download/R2S-OPoA-$romtype-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)/R2S-OPoA-$romtype-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)-ROM.zip
-		if [ -f /mnt/mmcblk0p2/R2S*.zip ]; then
+		if [ -f /overlay/R2S*.zip ]; then
 			echo -e '\e[92m昨天的固件已下载，准备解压\e[0m'
 		else
 			echo -e '\e[91m没找到最新的固件，脚本退出\e[0m'
@@ -62,39 +62,39 @@ else
 	fi
 fi
 # 开始刷机
-if [ -d /mnt/img ]; then
+if [ -d /overlay/img ]; then
 	echo -e '\e[92m清理文件夹\e[0m'
-	umount /mnt/img
+	umount /overlay/img
 	losetup -D
-	rm -rf /mnt/img
+	rm -rf /overlay/img
 fi
 unzip R2S*.zip
 rm R2S*.zip
-if [ -f /mnt/mmcblk0p2/artifact/FriendlyWrt*.img.gz ]; then
+if [ -f /overlay/artifact/FriendlyWrt*.img.gz ]; then
 	echo -e '\e[92m准备解压镜像文件\e[0m'
 	succes=1
-	pv /mnt/mmcblk0p2/artifact/FriendlyWrt*.img.gz | gunzip -dc > FriendlyWrt.img
+	pv /overlay/artifact/FriendlyWrt*.img.gz | gunzip -dc > FriendlyWrt.img
 fi
 if [ $succes -eq 1 ]; then
 	if [ $backup -eq 1 ]; then
 		lodev=$(losetup -f)
-		mkdir /mnt/img
-		losetup -o 100663296 $lodev /mnt/mmcblk0p2/FriendlyWrt.img
-		mount $lodev /mnt/img
+		mkdir /overlay/img
+		losetup -o 100663296 $lodev /overlay/FriendlyWrt.img
+		mount $lodev /overlay/img
 		echo -e '\e[92m解压已完成，准备编辑镜像文件，写入备份信息\e[0m'
-		cd /mnt/img
-		sysupgrade -b /mnt/img/back.tar.gz
+		cd /overlay/img
+		sysupgrade -b /overlay/img/back.tar.gz
 		tar zxf back.tar.gz
 		echo -e '\e[92m备份文件已经写入，移除挂载\e[0m'
 		rm back.tar.gz
 		cd /tmp
-		umount /mnt/img
+		umount /overlay/img
 		losetup -d $lodev
 	else
 		echo -e '\e[92m已跳过备份，将进行纯净安装\e[0m'
 	fi
 	echo -e '\e[92m准备重新打包\e[0m'
-	zstdmt /mnt/mmcblk0p2/FriendlyWrt.img -o /tmp/FriendlyWrtupdate.img.zst
+	zstdmt /overlay/FriendlyWrt.img -o /tmp/FriendlyWrtupdate.img.zst
 	echo -e '\e[92m打包完毕，准备刷机\e[0m'
 	if [ -f /tmp/FriendlyWrtupdate.img.zst ]; then
 		echo 1 > /proc/sys/kernel/sysrq
