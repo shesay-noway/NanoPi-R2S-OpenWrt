dccd186c359a354850cd9d5660d38e321e5a40de
msylgj <msylgj@vip.qq.com>
2020-05-06 23:10:55 +0800

    chuck版也加入守护脚本

--- scripts/checkwan.sh
+++ scripts/checkwan.sh
@@ -7,22 +7,22 @@ wan=`ifconfig |grep inet| sed -n '1p'|awk '{print $2}'|awk -F ':' '{print $2}'`
 while true
 do
 # do something
-if ping -w 1 -c 1 119.29.29.29; then #ping dns通则
+if ping -w 1 -c 1 119.29.29.29 >/dev/null 2>&1; then #ping dns通则
 	echo '网络正常'
 	tries=0
 else
 	sleep 1
-	if ping -w 1 -c 1 $wan; then #若ping网关通则
+	if ping -w 1 -c 1 $wan >/dev/null 2>&1; then #若ping网关通则
 		echo '网关正常'
 		tries=$((tries+1))
 	else #ping不通，重启防火墙
 		echo '网络崩溃重启防火墙'
-		/etc/init.d/firewall reload
+		/etc/init.d/firewall reload >/dev/null 2>&1
 		sleep 10
 	fi
 	if [ $tries -ge 5 ]; then #连续ping dns 5次失败，重启wan口
 		tries=0
-		/etc/init.d/network restart
+		/etc/init.d/network restart >/dev/null 2>&1
 		sleep 5
 	fi
 fi
