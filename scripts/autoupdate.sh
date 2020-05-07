e174003574c71fc3b829172d30e629ce91dcb7dd
msylgj <msylgj@vip.qq.com>
2020-05-08 00:20:57 +0800

    更换刷机脚本

--- scripts/autoupdate.sh
+++ scripts/autoupdate.sh
@@ -1,58 +1,168 @@
 #!/bin/sh
 
-# 默认备份
-backup=1
-succes=0
-# 在线or本地
-echo -e '\e[92m请选择更新固件方式\e[0m'
-echo -e '\e[92m  1.在线下载更新(默认)\e[0m'
-echo -e '\e[92m  2.在线下载更新且跳过备份\e[0m'
-echo -e '\e[92m  3.本地更新(固件上传至/tmp/upload目录)\e[0m'
-echo -e '\e[92m  4.本地更新且跳过备份(固件上传至/tmp/upload目录)\e[0m'
-read -p '请选择(1/2/3/4):' input
-if [ "$input" -eq 3 ] || [ "$input" -eq 4 ]; then
-	if [ -f /tmp/upload/R2S*.zip ]; then
-		echo -e '\e[92m已找到固件文件，准备解压\e[0m'
-		mv /tmp/upload/R2S*.zip /overlay
-		cd /overlay
-		if [ "$input" -eq 4 ]; then
+rom=0; 	#rom值若为0，则会出现可选菜单，也可手动改为1-3，将不会出现选项
+backup=0; 	#backup值若为0，则会出现可选菜单，也可手动改为1-3，将不会出现选项
+mode=0; 	#mode值若为0，则会出现可选菜单，也可手动改为1-3，将不会出现选项
+suffix=1; 	#判定升级文件名后缀，无需改动，保持1即可
+while [ $rom -eq 0 ]
+	do
+		echo
+		echo "...........欢迎使用 R2S 一键升级脚本.........."
+		echo " 1. 升级Chuck版"
+		echo
+		echo " 2. 升级QiuSimons版"
+		echo
+		echo " 3. 本地升级（固件以R2S*.zip或Friendly*.img.gz格式放在/tmp/upload目录，优先判定zip格式）"
+		echo
+		echo " 4. 输入zip格式固件下载地址"
+		echo
+		echo " 5. 输入img.gz格式固件下载地址"
+		echo
+		echo " 6. 退出"
+		echo
+		read -p "$(echo -e "请选择 [\e[95m1-6\e[0m]:")" rom
+		case $rom in
+		1)
+			rom=1;;
+		2)
+			rom=2;;
+		3)
+			rom=3;;
+		4)
+			rom=4
+			read -p "$(echo -e "\e[92m请输入固件下载地址\e[0m:")" address
+			;;
+		5)
+			rom=5
+			read -p "$(echo -e "\e[92m请输入固件下载地址\e[0m:")" address
+			;;
+		6)
+			exit 1
+			;;
+		*)
+			rom=0
+			echo
+			echo -e '\e[91m输入错误，请重新输入\e[0m'
+			;;
+		esac
+	done
+
+while [ $backup -eq 0 ]
+	do
+		echo
+		echo "...........欢迎使用 R2S 一键升级脚本.........."
+		echo " 1. 升级保留配置（同系列的固件直接升级推荐使用）"
+		echo
+		echo " 2. 特殊保留模式（只保留网口配置、防火墙、端口转发、DDNS和SSRP的数据，方便跨版本刷机）"
+		echo
+		echo " 3. 升级不保留配置"
+		echo
+		echo
+		read -p "$(echo -e "请选择 [\e[95m1-3\e[0m]，默认为1:")" backup
+		[[ -z $backup ]] && backup="1"
+		case $backup in
+		1)
+			backup=1;;
+		2)
+			backup=2;;
+		3)
+			backup=3;;
+		*)
 			backup=0
-		fi
-	else
-		echo -e '\e[92m没找到固件文件，脚本退出\e[0m'
+			echo
+			echo -e '\e[91m输入错误，请重新输入\e[0m'
+			;;
+		esac
+	done
+
+while [ $mode -eq 0 ]
+	do
+		echo
+		echo "...........欢迎使用 R2S 一键升级脚本.........."
+		echo " 1. 使用pigz刷机（速度更快）"
+		echo
+		echo " 2. 使用zstd刷机（理论上，更新成功率更高）"
+		echo
+		echo " 3. 不刷机，只保留上述修改的刷机文件（可以此制作适合自己已保留配置的刷机镜像，卡刷时救砖用）"
+		echo
+		echo
+		read -p "$(echo -e "请选择 [\e[95m1-3\e[0m]，默认为3:")" mode
+		[[ -z $mode ]] && mode="3"
+		case $mode in
+		1)
+			mode=1;;
+		2)
+			mode=2;;
+		3)
+			mode=3;;
+		*)
+			mode=0
+			echo
+			echo -e '\e[91m输入错误，请重新输入\e[0m'
+			;;
+		esac
+	done
+
+cd /overlay
+echo '检查依赖文件...'
+if ! type "unzip" > /dev/null; then
+	opkg update ; opkg install unzip
+	if ! type "unzip" > /dev/null; then
+		echo 'unzip安装失败，退出...'
 		exit 1
 	fi
-else
-	if [ "$input" -eq 2 ]; then
-		backup=0
+fi
+if ! type "pv" > /dev/null; then
+	opkg update ; opkg install pv
+	if ! type "pv" > /dev/null; then
+		echo 'pv安装失败，退出...'
+		exit 1
 	fi
-	echo -e '\e[92m请选择版本分支\e[0m'
-	echo -e '\e[92m  1.Chuck版(默认)\e[0m'
-	echo -e '\e[92m  2.QiuSimons版\e[0m'
-	read -p '请选择(1/2):' rom
-	if [ "$rom" -eq 2 ]; then
-		romtype='QiuSimons'
-	else
-		romtype='Chuck'
+fi
+if ! type "losetup" > /dev/null; then
+	opkg update ; opkg install losetup
+	if ! type "losetup" > /dev/null; then
+		echo 'losetup安装失败，退出...'
+		exit 1
 	fi
-	# 切换下载站点
-	read -p '是否从镜像站点下载?(解决无科学上网情况下下载慢的问题, y or n):' mirror
-	if [ "$mirror" = "y" ]; then
-		url='https://git.msylgj.workers.dev/https://github.com'
-		echo -e '\e[92m将从镜像站点下载\e[0m'
-	else
-		url='https://github.com'
-		echo -e '\e[92m从Github主站下载\e[0m'
+fi
+if [ $mode -eq 1 ] || [ $mode -eq 3 ]; then
+	if ! type "pigz" > /dev/null; then
+		if [ -f /www/pigz_2.4-1_aarch64_cortex-a53.ipk ]; then
+		opkg install /www/pigz_2.4-1_aarch64_cortex-a53.ipk
+		elif [ -f /tmp/upload/pigz_2.4-1_aarch64_cortex-a53.ipk ]; then
+		opkg install /www/pigz_2.4-1_aarch64_cortex-a53.ipk
+		else
+			rm pigz_2.4-1_aarch64_cortex-a53.ipk
+			wget https://github.com/lsl330/R2S-SCRIPTS/raw/master/pigz_2.4-1_aarch64_cortex-a53.ipk
+			opkg install pigz_2.4-1_aarch64_cortex-a53.ipk
+			cp pigz_2.4-1_aarch64_cortex-a53.ipk /www
+		fi
+		if ! type "pigz" > /dev/null; then
+			echo 'pigz安装失败，退出...'
+			exit 1
+		fi
 	fi
-	# 下载固件
-	cd /overlay
-	rm -rf artifact R2S*.zip FriendlyWrt*img*
-	wget $url/msylgj/NanoPi-R2S-OpenWrt/releases/download/R2S-OPoA-$romtype-$(date +%Y-%m-%d)/R2S-OPoA-$romtype-$(date +%Y-%m-%d)-ROM.zip
+fi
+if [ $mode -eq 2 ]; then
+	if ! type "zstd" > /dev/null; then
+		opkg update ; opkg install zstd
+		if ! type "zstd" > /dev/null; then
+			echo 'zstd安装失败，退出...'
+			exit 1
+		fi
+	fi
+fi
+
+rm -rf artifact R2S*.zip FriendlyWrt*img*
+
+if [ $rom -eq 1 ]; then	#下载Chuck固件
+	wget https://github.com/msylgj/NanoPi-R2S-OpenWrt/releases/download/R2S-OPoA-Chuck-$(date +%Y-%m-%d)/R2S-OPoA-Chuck-$(date +%Y-%m-%d)-ROM.zip
 	if [ -f /overlay/R2S*.zip ]; then
 		echo -e '\e[92m今天固件已下载，准备解压\e[0m'
 	else
-		echo -e '\e[91m今天的固件还没更新，尝试下载昨天的固件\e[0m'
-		wget $url/msylgj/NanoPi-R2S-OpenWrt/releases/download/R2S-OPoA-$romtype-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)/R2S-OPoA-$romtype-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)-ROM.zip
+		echo '今天的固件还没更新，尝试下载昨天的固件'
+		wget https://github.com/msylgj/NanoPi-R2S-OpenWrt/releases/download/R2S-OPoA-Chuck-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)/R2S-OPoA-Chuck-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)-ROM.zip
 		if [ -f /overlay/R2S*.zip ]; then
 			echo -e '\e[92m昨天的固件已下载，准备解压\e[0m'
 		else
@@ -61,48 +171,128 @@ else
 		fi
 	fi
 fi
-# 开始刷机
-if [ -d /overlay/img ]; then
-	echo -e '\e[92m清理文件夹\e[0m'
-	umount /overlay/img
-	losetup -D
-	rm -rf /overlay/img
-fi
-unzip R2S*.zip
-rm R2S*.zip
-if [ -f /overlay/artifact/FriendlyWrt*.img.gz ]; then
-	echo -e '\e[92m准备解压镜像文件\e[0m'
-	succes=1
-	pv /overlay/artifact/FriendlyWrt*.img.gz | gunzip -dc > FriendlyWrt.img
-fi
-if [ $succes -eq 1 ]; then
-	if [ $backup -eq 1 ]; then
-		lodev=$(losetup -f)
-		mkdir /overlay/img
-		losetup -o 100663296 $lodev /overlay/FriendlyWrt.img
-		mount $lodev /overlay/img
-		echo -e '\e[92m解压已完成，准备编辑镜像文件，写入备份信息\e[0m'
-		cd /overlay/img
-		sysupgrade -b /overlay/img/back.tar.gz
-		tar zxf back.tar.gz
-		echo -e '\e[92m备份文件已经写入，移除挂载\e[0m'
-		rm back.tar.gz
-		cd /tmp
-		umount /overlay/img
-		losetup -d $lodev
+
+if [ $rom -eq 2 ]; then	#下载QiuSimons固件
+	wget https://github.com/msylgj/NanoPi-R2S-OpenWrt/releases/download/R2S-OPoA-QiuSimons-$(date +%Y-%m-%d)/R2S-OPoA-QiuSimons-$(date +%Y-%m-%d)-ROM.zip
+	if [ -f /overlay/R2S*.zip ]; then
+		echo -e '\e[92m今天固件已下载，准备解压\e[0m'
 	else
-		echo -e '\e[92m已跳过备份，将进行纯净安装\e[0m'
+		echo '今天的固件还没更新，尝试下载昨天的固件'
+		wget https://github.com/msylgj/NanoPi-R2S-OpenWrt/releases/download/R2S-OPoA-QiuSimons-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)/R2S-OPoA-QiuSimons-$(date -d "@$(( $(busybox date +%s) - 86400))" +%Y-%m-%d)-ROM.zip
+		if [ -f /overlay/R2S*.zip ]; then
+			echo -e '\e[92m昨天的固件已下载，准备解压\e[0m'
+		else
+			echo -e '\e[91m没找到最新的固件，脚本退出\e[0m'
+			exit 1
+		fi
 	fi
-	echo -e '\e[92m准备重新打包\e[0m'
-	zstdmt /overlay/FriendlyWrt.img -o /tmp/FriendlyWrtupdate.img.zst
-	echo -e '\e[92m打包完毕，准备刷机\e[0m'
-	if [ -f /tmp/FriendlyWrtupdate.img.zst ]; then
-		echo 1 > /proc/sys/kernel/sysrq
-		echo u > /proc/sysrq-trigger || umount /
-		pv /tmp/FriendlyWrtupdate.img.zst | zstdcat | dd of=/dev/mmcblk0 conv=fsync
-		echo -e '\e[92m刷机完毕，正在重启...\e[0m'
-		echo b > /proc/sysrq-trigger
+fi
+
+if [ $rom -eq 3 ]; then	#上传本地rom
+	if [ -f /tmp/upload/R2S*.zip ]; then  #检测upload目录是否有zip升级文件
+		echo -e '\e[92m找到本地固件，准备解压\e[0m'
+		mv /tmp/upload/R2S*.zip /overlay/
+	elif [ -f /tmp/upload/Friendly*.img.gz ]; then	 #检测upload目录是否有img.gz升级文件
+		suffix=2
+		mv /tmp/upload/Friendly*.img.gz /overlay/FriendlyWrt-ROM.img.gz
+	elif [ -f /overlay/R2S*.zip ]; then  #检测upload目录是否有升级文件
+		echo -e '\e[92m找到本地固件，准备解压\e[0m'
+	else
+		echo -e '\e[91m没找到本地固件，脚本退出\e[0m'
+		exit 1
 	fi
+fi
+
+if [ $rom -eq 4 ]; then	#指定下载地址
+	wget $address -O /overlay/R2S-ROM.zip
+	if [ -f /overlay/R2S*.zip ]; then
+		echo -e '\e[92m固件已下载，准备解压\e[0m'
+	else
+		echo -e '\e[91m指定位置没找到固件，脚本退出\e[0m'
+		exit 1
+	fi
+fi
+
+if [ $rom -eq 5 ]; then	#指定下载地址
+	wget $address -O /overlay/FriendlyWrt-ROM.img.gz
+	if [ -f /overlay/FriendlyWrt-ROM.img.gz ]; then
+		suffix=2
+		echo -e '\e[92m固件已下载，准备解压\e[0m'
+	else
+		echo -e '\e[91m指定位置没找到固件，脚本退出\e[0m'
+		exit 1
+	fi
+fi
+
+if [ $suffix -eq 1 ]; then	#zip格式固件，进行解压
+	unzip R2S*.zip
+	rm R2S*.zip
+fi
+
+if [ -f /overlay/artifact/FriendlyWrt*.img.gz ]; then  #统一解压固件路径
+	pv /overlay/artifact/FriendlyWrt*.img.gz | gunzip -dc > FriendlyWrt.img
+	echo -e '\e[92m准备解压镜像文件\e[0m'
+elif [ -f /overlay/FriendlyWrt*.img.gz ]; then
+	pv /overlay/FriendlyWrt*.img.gz | gunzip -dc > FriendlyWrt.img
+	echo -e '\e[92m准备解压镜像文件\e[0m'
+fi
+lodev=$(losetup -f)
+mkdir /overlay/img
+losetup -o 100663296 $lodev /overlay/FriendlyWrt.img
+mount $lodev /overlay/img
+echo -e '\e[92m解压已完成，准备编辑镜像文件，写入备份信息\e[0m'
+cd /overlay/img
+if [ $backup -eq 1 ]; then
+	sysupgrade -b /overlay/img/back.tar.gz
+	tar zxf back.tar.gz
+	echo -e '\e[92m备份文件已经写入，移除挂载\e[0m'
+	rm back.tar.gz
+elif [ $backup -eq 2 ]; then
+	cp -f /etc/config/network /overlay/img/etc/config/; #网络配置文件
+	rm -rf /overlay/img/etc/board.d
+	cp -f /etc/board.d /overlay/img/etc/
+	cp -f /etc/board.json /overlay/img/etc/
+	cp -f /etc/config/aria2 /overlay/img/etc/config/; #aria2配置文件
+	cp -f /etc/config/ddns /overlay/img/etc/config/; #ddns配置文件
+	cp -f /etc/config/frps /overlay/img/etc/config/; #frps配置文件
+	cp -f /etc/passwd /overlay/img/etc/; #账号文件配置文件
+	cp -f /etc/shadow /overlay/img/etc/; #账号密码配置文件
+	cp -f /etc/config/firewall /overlay/img/etc/config/; #防火墙及端口转发配置文件
+	cp -f /etc/config/shadowsocksr /overlay/img/etc/config/; #ssrp配置文件
+	cp -f /etc/config/netflixip.list /overlay/img/etc/config/; #ssrp配置文件
+	cp -f /etc/china_ssr.txt /overlay/img/etc/; #ssrp配置文件
+	mkdir /overlay/img/etc/dnsmasq.ssr; #ssrp配置文件
+	cp -f /etc/dnsmasq.ssr/gfw_list.conf /overlay/img/etc/dnsmasq.ssr/; #ssrp配置文件
 else
-	echo -e '\e[92m镜像文件不匹配, 脚本退出\e[0m'
+	echo -e '\e[92m升级文件已经写入，移除挂载\e[0m'
+fi
+cd /tmp
+umount /overlay/img
+losetup -d $lodev
+echo -e '\e[92m准备重新打包\e[0m'
+if [ $mode -eq 3 ]; then
+	mkdir /tmp/upload
+	pv /overlay/FriendlyWrt.img | pigz > /tmp/upload/FriendlyWrtupdate.img.gz
+	echo -e '\e[92m刷机镜像已保存在/tmp/upload目录，请及时导出\e[0m'
+	exit 1
+fi
+if [ $mode -eq 1 ]; then
+	pv /overlay/FriendlyWrt.img | pigz --fast > /tmp/FriendlyWrtupdate.img.gz
+else
+	zstdmt /overlay/FriendlyWrt.img -o /tmp/FriendlyWrtupdate.img.zst
+fi
+echo -e '\e[92m打包完毕，准备刷机\e[0m'
+if [ -f /tmp/FriendlyWrtupdate.img.gz ]; then
+	echo 1 > /proc/sys/kernel/sysrq
+	echo u > /proc/sysrq-trigger || umount /
+	pv /tmp/FriendlyWrtupdate.img.gz | gunzip -dc > /dev/mmcblk0
+	echo -e '\e[92m刷机完毕，正在重启...\e[0m'
+	echo b > /proc/sysrq-trigger
+fi
+if [ -f /tmp/FriendlyWrtupdate.img.zst ]; then
+	echo 1 > /proc/sys/kernel/sysrq
+	echo u > /proc/sysrq-trigger || umount /
+	pv /tmp/FriendlyWrtupdate.img.zst | zstdcat | dd of=/dev/mmcblk0 conv=fsync
+	echo -e '\e[92m刷机完毕，正在重启...稍等片刻后重新登录\e[0m'
+	echo b > /proc/sysrq-trigger
 fi
